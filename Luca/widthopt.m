f7d99b564eef5bc2661ce4ceb3fb8b8f58190f8e
routard97 <82665819+routard97@users.noreply.github.com>
2021-12-20 17:52:21 +0100

    Update widthopt.m
    ho preallocato delle variabili, corretto una porzione di codice che avevo sbagliato e tentato a variare lo spessore, senza risultati degni di nota

--- Luca/widthopt.m
+++ Luca/widthopt.m
@@ -6,25 +6,32 @@ epsr=4.800; %FR4 relative permettivity
 Rin=50; %input resistance, requested
 k0=2*pi*lambda;
 beta=2*pi*(sqrt(epsr)/lambda);
-L =0.03;
-W=linspace(0.001,0.01,1000);
+L =0.07;
+W=linspace(0.001,0.1,1000);
 Lf=zeros(1,length(W));
+Lfun=zeros(1,length(W));
+Dm=zeros(1,length(W));
+Dmun=zeros(1,length(W));
+BWH=zeros(1,length(W));
+BWHun=zeros(1,length(W));
 for i=1:length(W)
 Gf=W(i)/(120*lambda)*(1+0.5*(k0*h)^2);
-Rr=1/Gf; %radiation resistance for the folded patch, calculated from the Gf, that depends on the W width
+Rr=1/Gf; %radiation resistance for the folded patch, which is twice the radiation resistance of a normal patch
 Lfeed=L/pi*acos(sqrt(Rin/Rr));
 Lf(i)=abs(Lfeed);
-LfM=max(Lf);
-Lfun(i)=Lf(i)/LfM;
 Dmax=2/15*((W(i)/lambda)^2)*Rr;
 Dm(i)=Dmax;
-DmM=max(Dm);
-Dmun(i)=Dm(i)/DmM;
 BWh=2*acos(sqrt(1/(2+k0*W(i))));
 BWH(i)=BWh;
+end 
+for i=1:length(W)
+    LfM=max(Lf);
+Lfun(i)=Lf(i)/LfM;
+DmM=max(Dm);
+Dmun(i)=Dm(i)/DmM;
 BWHM=max(BWH);
 BWHun(i)=BWH(i)/BWHM;
-end 
+end
 subplot(2,2,1);
 plot(W,Lf,'b',W,L,'b--');
 title('feed distance');
@@ -33,7 +40,7 @@ plot(W,BWH,'g');
 title('Beam Width H-cut');
 subplot(2,2,3);
 plot(W,Dm,'r--'); 
-title('Directibvity');
+title('Directivity');
 subplot(2,2,4);
-plot(W,Lfun,'b',W,Dmun,'r',W,BWHun,'g');
+plot(W,Lfun,'b',W,Dmun,'r--',W,BWHun,'g');
 title('Comparison after scaling');
