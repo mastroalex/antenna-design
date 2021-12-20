dcb64e0e66780cd9ba7a7e1cb92f2da9ef37150e
routard97 <82665819+routard97@users.noreply.github.com>
2021-12-20 18:41:43 +0100

    Update widthopt.m
    ho provato ad aggiungere un ciclo facendo variare L, non sembrano esserci variazione sull'ordine di grandezza. si nota in particolare nel grafico in fondo a destra di ogni ciclo che la variazione di L non influisca l'andamento della distanza del feed in funzione di W

--- Luca/widthopt.m
+++ Luca/widthopt.m
@@ -6,7 +6,7 @@ epsr=4.800; %FR4 relative permettivity
 Rin=50; %input resistance, requested
 k0=2*pi*lambda;
 beta=2*pi*(sqrt(epsr)/lambda);
-L =0.07;
+L =linspace(0.01,0.1,5);
 W=linspace(0.001,0.1,1000);
 Lf=zeros(1,length(W));
 Lfun=zeros(1,length(W));
@@ -14,17 +14,18 @@ Dm=zeros(1,length(W));
 Dmun=zeros(1,length(W));
 BWH=zeros(1,length(W));
 BWHun=zeros(1,length(W));
+for j=1:length(L)
 for i=1:length(W)
 Gf=W(i)/(120*lambda)*(1+0.5*(k0*h)^2);
 Rr=1/Gf; %radiation resistance for the folded patch, which is twice the radiation resistance of a normal patch
-Lfeed=L/pi*acos(sqrt(Rin/Rr));
+Lfeed=L(j)/pi*acos(sqrt(Rin/Rr));
 Lf(i)=abs(Lfeed);
 Dmax=2/15*((W(i)/lambda)^2)*Rr;
 Dm(i)=Dmax;
 BWh=2*acos(sqrt(1/(2+k0*W(i))));
 BWH(i)=BWh;
 end 
-for i=1:length(W)
+for i=1:length(W) 
     LfM=max(Lf);
 Lfun(i)=Lf(i)/LfM;
 DmM=max(Dm);
@@ -33,7 +34,7 @@ BWHM=max(BWH);
 BWHun(i)=BWH(i)/BWHM;
 end
 subplot(2,2,1);
-plot(W,Lf,'b',W,L,'b--');
+plot(W,Lf,'b',W,L(j),'r--',W,L(j)/2,'r');
 title('feed distance');
 subplot(2,2,2);
 plot(W,BWH,'g');
@@ -44,3 +45,5 @@ title('Directivity');
 subplot(2,2,4);
 plot(W,Lfun,'b',W,Dmun,'r--',W,BWHun,'g');
 title('Comparison after scaling');
+figure
+end
